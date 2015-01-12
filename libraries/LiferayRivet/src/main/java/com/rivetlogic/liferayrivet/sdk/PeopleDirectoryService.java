3da2d63f6b7aff8e9d0aa4f4480bc98f4a32d121
thomas.lorenz <thomas.lorenz@rivetlogic.com>
2015-01-12 18:14:51 -0500

    custom sdk

---
+++ libraries/LiferayRivet/src/main/java/com/rivetlogic/liferayrivet/sdk/PeopleDirectoryService.java
@@ -0,0 +1,7 @@
+package com.rivetlogic.liferayrivet.sdk;
+
+/**
+ * Created by lorenz on 1/12/15.
+ */
+public class PeopleDirectoryService {
+}
--- libraries/LiferayRivet/src/main/java/com/rivetlogic/liferayrivet/ui/login/LoginView.java
+++ libraries/LiferayRivet/src/main/java/com/rivetlogic/liferayrivet/ui/login/LoginView.java
@@ -1,4 +1,4 @@
-package com.rivetlogc.liferayrivet.ui.login;
+package com.rivetlogic.liferayrivet.ui.login;
 
 import android.content.Context;
 import android.content.res.TypedArray;
